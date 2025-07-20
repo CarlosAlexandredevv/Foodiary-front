806e057b0aea58844322d077cd866712c5492560
Carlos Alexandre <alexfilho062005@gmail.com>
2025-07-20 10:54:46 -0300

    :lipstick: feat: daily stats component

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { HomeHeader } from './components/HomeHeader';
 import './styles/global.css';
 import { DateSwitcher } from './components/DateSwitcher';
+import { DailyStats } from './components/DailyStats';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -39,6 +40,28 @@ export default function App() {
       <SafeAreaProvider>
         <HomeHeader />
         <DateSwitcher />
+        <View className="mt-2">
+          <DailyStats
+            calories={{
+              current: 500,
+              goal: 2500,
+            }}
+            proteins={{
+              current: 2000,
+              goal: 2500,
+            }}
+            carbohydrates={{
+              current: 500,
+              goal: 2500,
+            }}
+            fats={{
+              current: 500,
+              goal: 2500,
+            }}
+          />
+        </View>
+
+        <View className="h-px bg-gray-200 mt-7" />
       </SafeAreaProvider>
     </View>
   );
