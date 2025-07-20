e606e023bd7c40738d80778104fd03403efa78ca
Carlos Alexandre <alexfilho062005@gmail.com>
2025-07-20 14:58:34 -0300

    :sparkles: protected route

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -18,18 +18,30 @@ export default function RootLayout() {
     HostGrotesk_600SemiBold,
     HostGrotesk_700Bold,
   });
+
   useEffect(() => {
     if (loaded || error) {
       SplashScreen.hideAsync();
     }
   }, [loaded, error]);
+
   if (!loaded && !error) {
     return null;
   }
+
+  const isLoggedIn = false;
+
   return (
     <SafeAreaProvider>
-      {' '}
-      <Stack screenOptions={{ headerShown: false }} />{' '}
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Protected guard={isLoggedIn}>
+          <Stack.Screen name="(private)" />
+        </Stack.Protected>
+
+        <Stack.Protected guard={!isLoggedIn}>
+          <Stack.Screen name="(public)" />
+        </Stack.Protected>
+      </Stack>
     </SafeAreaProvider>
   );
 }
